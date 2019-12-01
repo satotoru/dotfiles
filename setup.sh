11b612b14cd99fdd92272a172f8f5fd606cd2689
satotoru <6828551+satotoru@users.noreply.github.com>
2019-12-01 14:23:01 +0900

    setupスクリプト

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir ~/.config
+
+ln -s ./.vimrc ~/.vimrc
+ln -s ./.vim ~/.vim
+ln -s ./.tmux.conf ~/.tmux.conf
+
+mkdir ~/.config/fish
+ln -s ./config.fish ~/.config/fish/config.fish
+ln -s ./fishfile ~/.config/fish/fishfile
+
+ln -s ./.gitconfig ~/.gitconfig
+ln -s ./.gitignore_global ~/.gitignore_global
+ln -s ./karabiner ~/.config/karabiner
