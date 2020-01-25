2ed43137e3463f8ebea01c97c5e0940b89bd5329
satotoru <satotoru2000@gmail.com>
2020-01-25 23:51:57 +0900

    Ankiをフローティングに設定

--- linux/.config/awesome/rc.lua
+++ linux/.config/awesome/rc.lua
@@ -502,6 +502,7 @@ awful.rules.rules = {
           "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
           "Wpa_gui",
           "veromix",
+          "Anki",
           "xtightvncviewer"},
 
         -- Note that the name property shown in xprop might be set slightly after creation of the client
