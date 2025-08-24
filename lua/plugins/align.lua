73922b22c0abd6502ef69783dc84fc749aed78b9
satotoru <satotoru2000@gmail.com>
2025-08-24 17:00:13 +0900

    不要なプラグインを削除

--- lua/plugins/align.lua
+++
@@ -1,8 +0,0 @@
--- テキスト整列ツール (vim-easy-align)
-return {
-  "junegunn/vim-easy-align",
-  cmd = { "EasyAlign" },
-  keys = {
-    { "ga", "<Plug>(EasyAlign)", mode = { "n", "x" }, desc = "Easy align" },
-  },
-}
