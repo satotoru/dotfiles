ceed8c795432a7f51a83726f06f6df8c05139331
satotoru <6828551+satotoru@users.noreply.github.com>
2025-08-27 19:29:47 +0900

    Mason経由のLSPがうまく動作しないため削除し手動でLSPを入れる方式に変更

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,11 +1,24 @@
 -- LSP関連プラグインの定義 (サンプル)
 return {
-  "mason-org/mason-lspconfig.nvim",
-  opts = {
-    ensure_installed = { "lua_ls" },
-  },
-  dependencies = {
-    { "mason-org/mason.nvim", opts = {} },
-    "neovim/nvim-lspconfig",
-  },
+  "neovim/nvim-lspconfig",
+  config = function ()
+    local lspconfig = require('lspconfig')
+
+    -- lua
+    lspconfig.lua_ls.setup({})
+
+    -- ruby-lsp
+    lspconfig.ruby_lsp.setup({
+      -- 起動コマンドをカスタマイズ
+      cmd = { "bundle", "exec", "ruby-lsp" },
+    })
+
+    -- ts_ls
+    lspconfig.ts_ls.setup({
+      cmd = {
+        "/Users/toru.sato/.nodenv/versions/22.18.0/bin/typescript-language-server",
+        "--stdio"
+      }
+    })
+  end
 }
